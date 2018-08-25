9ffe1484f94425dca0b19d8dbfe552f3140b5800
sbwml <984419930@qq.com>
2018-08-25 20:59:33 +0800

    first commit

---
+++ msm8998/libc2dcolorconvert/Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+        C2DColorConverter.cpp
+
+LOCAL_C_INCLUDES := \
+    $(TARGET_OUT_HEADERS)/adreno
+LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/qcom/display
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+
+LOCAL_SHARED_LIBRARIES := liblog libdl libutils
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE := libc2dcolorconvert
+
+LOCAL_VENDOR_MODULE := true
+
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+
+LOCAL_HEADER_LIBRARIES += libhardware_headers
+include $(BUILD_SHARED_LIBRARY)
